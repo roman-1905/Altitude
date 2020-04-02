4f49997f9970b4dcf79398b4bc4c3d49f6132d97
mdfkbtc <mdfkbtc@veles.network>
2020-04-02 18:10:42 +0200

    Add balance bar to navbar , add 'Jura' font

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { WalletService } from 'app/metrix/providers/wallet.service';
+import { CurrencyService } from 'app/providers/currency.service';
+
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
@@ -9,6 +12,8 @@ export class NavBarComponent {
 
 
   constructor(
+  	public wallet: WalletService,
+    public currencyService: CurrencyService
   ) {
 
   }
