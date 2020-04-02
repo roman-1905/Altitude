ca9783bf89c1433a1bed3e45fbf492ff0602c45b
mdfkbtc <mdfkbtc@veles.network>
2020-04-02 16:55:13 +0200

    Add Veles navbar , remove existing sidebar/appbar

--- src/app/app-components.module.ts
+++ src/app/app-components.module.ts
@@ -3,12 +3,12 @@ import { ContextMenuComponent } from './components/context-menu/context-menu.com
 import { ContextMenuService } from './components/context-menu/context-menu.service';
 import { TextinputComponent } from './components/textinput/textinput.component';
 import { TextareaComponent } from './components/textarea/textarea.component';
-import { AppBarComponent } from './components/app-bar/app-bar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LoaderComponent } from './components/loader/loader.component';
 
 export const componentDeclarations = [
     LoaderComponent,
-    AppBarComponent,
+    NavBarComponent,
     ContextMenuComponent,
     TextinputComponent,
     TextareaComponent,
--- src/app/components/nav-bar/nav-bar.component.spec.ts
+++ src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AppBarComponent } from './app-bar.component';
+import { NavBarComponent } from './nav-bar.component';
 
-describe('AppBarComponent', () => {
-  let component: AppBarComponent;
-  let fixture: ComponentFixture<AppBarComponent>;
+describe('NavBarComponent', () => {
+  let component: NavBarComponent;
+  let fixture: ComponentFixture<NavBarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AppBarComponent ]
+      declarations: [ NavBarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AppBarComponent);
+    fixture = TestBed.createComponent(NavBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
